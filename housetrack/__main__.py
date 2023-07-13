1c4ca2cdc549c695140eddf68a2eeb8c61405bf5
joostsijm <joostsijm@gmail.com>
2023-07-13 09:50:05 +0200

    refact: Move houselist etl to directory

--- housetrack/__main__.py
+++ housetrack/__main__.py
@@ -5,9 +5,7 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-from extract import Extract
-from transform import Transform
-from load import Load
+import houselist
 
 
 load_dotenv()
@@ -22,15 +20,15 @@ HTML_PATH = Path.cwd() / "mijn_gevonden_woningen.html"
 def main():
     """Method to initialize the application"""
 
-    extract = Extract(MOVE_USERNAME, MOVE_PASSWORD)
+    extract = houselist.Extract(MOVE_USERNAME, MOVE_PASSWORD)
     extracted_data = extract.execute()
     with open(HTML_PATH, "w", encoding="utf-8") as file:
         file.write(extracted_data)
 
-    transform = Transform(HTML_PATH)
+    transform = houselist.Transform(HTML_PATH)
     transformed_data = transform.execute()
 
-    load = Load(AIRTABLE_TOKEN, AIRTABLE_BASE, AIRTABLE_TABLE)
+    load = houselist.Load(AIRTABLE_TOKEN, AIRTABLE_BASE, AIRTABLE_TABLE)
     loaded_data = load.execute(transformed_data)
     print(loaded_data)
 
