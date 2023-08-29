552855ea24bf4feee99991710aa6c97c48183cfa
joostsijm <joostsijm@gmail.com>
2023-08-29 21:50:25 +0200

    feat: Add autoscrolling option

--- housetrack/houselist/extract.py
+++ housetrack/houselist/extract.py
@@ -18,37 +18,63 @@ class Extract:
     __password: str = None
     __html_path: Optional[str] = None
 
-    def __init__(self, username: str, password: str, html_path: Optional[str] = None):
+    def __init__(
+        self,
+        username: str,
+        password: str,
+        html_path: Optional[str] = None,
+        wait_time: Optional[str] = 2,
+    ):
         self.__username = username
         self.__password = password
         self.__html_path = html_path
+        self.__wait_time = wait_time
+        self.__driver = webdriver.Firefox()
 
     def execute(self):
         """Extract houses to file"""
-        driver = webdriver.Firefox()
         try:
-            driver.get("https://move.nl/login")
-            assert "Move" in driver.title
-            username_input = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.ID, "username"))
-            )
-            username_input.clear()
-            username_input.send_keys(self.__username)
+            self.login()
+            self.scroll_to_bottom()
 
-            password_input = driver.find_element(By.ID, "password")
-            password_input.clear()
-            password_input.send_keys(self.__password)
-
-            button = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
-            ActionChains(driver).click(button).perform()
-
-            WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "ant-spin-container"))
-            )
-            time.sleep(2)
             if self.__html_path:
                 with open(self.__html_path, "w", encoding="utf-8") as file:
-                    file.write(driver.page_source)
-            return driver.page_source
+                    file.write(self.__driver.page_source)
+            return self.__driver.page_source
         finally:
-            driver.quit()
+            self.__driver.quit()
+
+    def login(self):
+        self.__driver.get("https://move.nl/login")
+        assert "Move" in self.__driver.title
+        username_input = WebDriverWait(self.__driver, 10).until(
+            EC.presence_of_element_located((By.ID, "username"))
+        )
+        username_input.clear()
+        username_input.send_keys(self.__username)
+
+        password_input = self.__driver.find_element(By.ID, "password")
+        password_input.clear()
+        password_input.send_keys(self.__password)
+
+        button = self.__driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
+        ActionChains(self.__driver).click(button).perform()
+
+        WebDriverWait(self.__driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "ant-spin-container"))
+        )
+        time.sleep(self.__wait_time)
+
+    def scroll_to_bottom(self):
+        last_height = self.__driver.execute_script("return document.body.scrollHeight")
+        while True:
+            self.__driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight);"
+            )
+            time.sleep(self.__wait_time)
+            new_height = self.__driver.execute_script(
+                "return document.body.scrollHeight"
+            )
+            if new_height == last_height:
+                break
+            last_height = new_height
